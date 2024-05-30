c0e4f20143b8581f2fccdccf9a34f91602273696
keruzam <mazurek.zg@gmail.com>
2024-05-30 17:40:03 +0200

    MyFaces + Primefaces13 - test

--- src/main/java/pl/keruzam/springboot/WebSecurityConfig.java
+++ src/main/java/pl/keruzam/springboot/WebSecurityConfig.java
@@ -18,11 +18,11 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/", "/index.xhtml").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
-                        .loginPage("/login")
+                        .loginPage("/index.xhtml")
                         .permitAll()
                 )
                 .logout((logout) -> logout.permitAll());
